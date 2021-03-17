574c745bb900b8004da4541381cc447d6290434a
Lucas <lucas.rosenorn@outlook.dk>
2021-03-17 15:48:49 +0100

    initial commit

---
+++ src/main/java/no/lagalt/lagaltapi/LagaltapiApplication.java
@@ -0,0 +1,13 @@
+package no.lagalt.lagaltapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagaltapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagaltapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/lagalt/lagaltapi/LagaltapiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.lagalt.lagaltapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
