d0f6590ba331b741b04e319da049ba2b618895cc
Lucas <lucas.rosenorn@outlook.dk>
2021-04-02 15:04:13 +0200

    Feat: implemented initial Auth configuration for Firebase

---
+++ src/main/java/no/lagalt/lagaltapi/auth/models/CookieProperties.java
@@ -0,0 +1,57 @@
+package no.lagalt.lagaltapi.auth.models;
+
+public class CookieProperties {
+    String domain;
+    String path;
+    boolean httpOnly;
+    boolean secure;
+    int maxAgeInMinutes;
+
+    public CookieProperties(String domain, String path, boolean httpOnly, boolean secure, int maxAgeInMinutes) {
+        this.domain = domain;
+        this.path = path;
+        this.httpOnly = httpOnly;
+        this.secure = secure;
+        this.maxAgeInMinutes = maxAgeInMinutes;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public boolean isHttpOnly() {
+        return httpOnly;
+    }
+
+    public void setHttpOnly(boolean httpOnly) {
+        this.httpOnly = httpOnly;
+    }
+
+    public boolean isSecure() {
+        return secure;
+    }
+
+    public void setSecure(boolean secure) {
+        this.secure = secure;
+    }
+
+    public int getMaxAgeInMinutes() {
+        return maxAgeInMinutes;
+    }
+
+    public void setMaxAgeInMinutes(int maxAgeInMinutes) {
+        this.maxAgeInMinutes = maxAgeInMinutes;
+    }
+}
