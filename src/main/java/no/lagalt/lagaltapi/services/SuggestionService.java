08b04d67fd058a3b37db1a3cb6ef219e7e2cff59
Vytautas Zarauskas <v.zarauskas@gmail.com>
2021-04-07 20:37:10 +0200

    Merge: Added other active projects to suggestions when a user has no related projects to be suggested, some cleanup and minor variable name refactoring (#13)
    * Chore: Changed database function name from get_search_results to get_all_active_projects for readability
    
    * Chore: Deleted unused injections
    
    * Feat: Added other active projects to suggestions when a user has no related projects to be suggested
    
    * Chore: Deleted System.out.println() statements
    
    Co-authored-by: Vytautas Zarauskas <Vytautas.Zarauskas@dk.experis.com>

--- src/main/java/no/lagalt/lagaltapi/services/SuggestionService.java
+++ src/main/java/no/lagalt/lagaltapi/services/SuggestionService.java
@@ -19,27 +19,14 @@ public class SuggestionService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    @Autowired
-    private UsersProjectsRepository usersProjectsRepository;
-
-    @Autowired
-    private ClickedProjectsRepository clickedProjectsRepository;
-
-    @Autowired
-    private ViewedProjectsRepository viewedProjectsRepository;
-
     public ResponseEntity<Set<Project>> getSuggestions(long userId) {
         Set<Project> suggestedProjects = new LinkedHashSet<>();
 
         // Extracting relevant data
         Set<Long> suggestionsFromContributedProjects = userRepository.getSuggestionsFromContributedProjects(userId);
-        System.out.println(suggestionsFromContributedProjects);
         Set<Long> suggestionsFromClickedProjects = userRepository.getSuggestionsFromClickedProjects(userId);
-        System.out.println(suggestionsFromClickedProjects);
         Set<Long> userProjectMatchingSkills = userRepository.findUserProjectMatchingSkillsByUserId(userId);
-        System.out.println(userProjectMatchingSkills);
         Set<Long> suggestionsFromViewedProjects = userRepository.getSuggestionsFromViewedProjects(userId);
-        System.out.println(suggestionsFromViewedProjects);
 
         // Collecting suggestions to return
         if (suggestionsFromContributedProjects.size() > 0 && !suggestionsFromContributedProjects.contains(null)) {
@@ -53,6 +40,14 @@ public class SuggestionService {
         }
         if (suggestionsFromViewedProjects.size() > 0 && !suggestionsFromViewedProjects.contains(null)) {
             suggestionsFromViewedProjects.forEach(projectId -> suggestedProjects.add(projectRepository.findById(projectId).get()));
+        }
+
+        // If there are no matching projects, some other active projects are suggested
+        if (suggestedProjects.size() == 0) {
+            if (projectRepository.existsActiveProjects()) {
+                Set<Project> userUnrelatedActiveProjects = projectRepository.getUserUnrelatedActiveProjects(userId);
+                return new ResponseEntity<>(userUnrelatedActiveProjects, HttpStatus.OK);
+            }
         }
 
         return new ResponseEntity<>(suggestedProjects, HttpStatus.OK);
