d3a51cc7bd1afef4f340079d6e65ba2ca095023b
Vytautas Zarauskas <Vytautas.Zarauskas@dk.experis.com>
2021-04-07 11:04:25 +0200

    Chore: Changed database function name from get_search_results to get_all_active_projects for readability

--- src/main/java/no/lagalt/lagaltapi/repositories/ProjectRepository.java
+++ src/main/java/no/lagalt/lagaltapi/repositories/ProjectRepository.java
@@ -14,6 +14,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query(value = "SELECT EXISTS(SELECT * FROM projects WHERE is_active = true)", nativeQuery = true)
     boolean existsActiveProjects();
 
-    @Query(value = "SELECT * FROM get_search_results(?1, ?2, ?3, ?4)", nativeQuery = true)
+    @Query(value = "SELECT * FROM get_all_active_projects(?1, ?2, ?3, ?4)", nativeQuery = true)
     Set<Project> getAllActiveProjects(String industry, String status, String search, short limit);
 }
