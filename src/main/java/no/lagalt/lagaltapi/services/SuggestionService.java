75761487452d28ace6a592307998aadd017567ac
Vytautas Zarauskas <Vytautas.Zarauskas@dk.experis.com>
2021-04-09 13:38:20 +0200

    Chore: Removed unused imports

--- src/main/java/no/lagalt/lagaltapi/services/SuggestionService.java
+++ src/main/java/no/lagalt/lagaltapi/services/SuggestionService.java
@@ -1,7 +1,8 @@
 package no.lagalt.lagaltapi.services;
 
 import no.lagalt.lagaltapi.models.Project;
-import no.lagalt.lagaltapi.repositories.*;
+import no.lagalt.lagaltapi.repositories.ProjectRepository;
+import no.lagalt.lagaltapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
