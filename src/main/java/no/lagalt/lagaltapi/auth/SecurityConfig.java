5102b4f7f6f54e4176f5ce47340b51c200187f97
Lucas <lucas.rosenorn@outlook.dk>
2021-04-09 17:20:54 +0200

    Fix: Added front-end to origin

--- src/main/java/no/lagalt/lagaltapi/auth/SecurityConfig.java
+++ src/main/java/no/lagalt/lagaltapi/auth/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedHeaders(restSecProps.getAllowedHeaders());
         configuration.setAllowCredentials(restSecProps.isAllowCredentials());
         configuration.setExposedHeaders(restSecProps.getExposedHeaders());
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("https://lagaltno.herokuapp.com/");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
