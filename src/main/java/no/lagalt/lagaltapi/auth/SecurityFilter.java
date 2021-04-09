5fba4724d0d9547203196b250a21b2ac983798b5
Lucas <lucas.rosenorn@outlook.dk>
2021-04-09 17:35:31 +0200

    Fix: added Access-Control-Allow-Origin to filter.

--- src/main/java/no/lagalt/lagaltapi/auth/SecurityFilter.java
+++ src/main/java/no/lagalt/lagaltapi/auth/SecurityFilter.java
@@ -39,6 +39,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         verifyToken(request);
         filterChain.doFilter(request, response);
     }
